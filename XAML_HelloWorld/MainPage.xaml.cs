a257b4e307d293342b52f88c7f369f8dfe84a5d4
OsadaMasako <217bssr@gmail.com>
2019-01-18 23:43:52 +0900

    Hello, UWP

--- XAML_HelloWorld/MainPage.xaml.cs
+++ XAML_HelloWorld/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,14 +18,48 @@ using Windows.UI.Xaml.Navigation;
 
 namespace XAML_HelloWorld
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+	public sealed partial class MainPage : Page
+	{
+		public MainPageDataClass MainPageData = new MainPageDataClass();
+
+		public MainPage()
+		{
+			this.InitializeComponent();
+
+		}
+	}
+
+	public class MainPageDataClass
+	{
+		public string Libretto { get; set; } = string.Empty;
+		public string TitleLabel { get; set; } = "しゃべらせてみる？";
+		public string ButtonLabel { get; set; } = "Speak!";
+
+
+
+		public bool IsEmptyLibretto() => string.IsNullOrWhiteSpace(this.Libretto) ? false : true;
+
+		public async void Button_Click(object sernder, RoutedEventArgs e)
+		{
+			if (e == null)
+			{
+				throw new ArgumentNullException(nameof(e));
+			}
+
+			if (!IsEmptyLibretto())
+			{
+				Libretto = "文章を 入れてよ！";
+			}
+
+			MediaElement mediaElement = new MediaElement();
+			var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+			Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(Libretto);
+			mediaElement.SetSource(stream, stream.ContentType);
+			mediaElement.Play();
+
+		}
+	}
 }
